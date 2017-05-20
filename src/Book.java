a1b9fb948d1db2c0e819c9fe795690d349aedd8e
Andre <andrebrreis@gmail.com>
2017-05-20 16:33:24 +0100

    Add book title

--- src/Book.java
+++ src/Book.java
@@ -6,17 +6,23 @@ import java.util.Date;
  */
 public class Book {
 
+    private String title;
     private double length, height;
     private String author;
     private Date publicationDate;
     ArrayList<String> genres;
 
-    public Book(double length, double height, String author, Date publicationDate, ArrayList<String> genres) {
+    public Book(String title, double length, double height, String author, Date publicationDate, ArrayList<String> genres) {
+        this.title = title;
         this.length = length;
         this.height = height;
         this.author = author;
         this.publicationDate = publicationDate;
         this.genres = genres;
+    }
+
+    public String getTitle() {
+        return title;
     }
 
     public String getAuthor() {
